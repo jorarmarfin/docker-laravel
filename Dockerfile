efd2e7ead2b7ac94648c2a380fe582f152c7ebcd
Luis Mayta <luis.mayta@gmail.com>
2018-08-28 10:45:37 -0500

    local

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM php:7.2-apache
 MAINTAINER Luis Mayta <luis.mayta@gmail.com>
-COPY ./files/test.php /var/www/html
+RUN docker-php-ext-install php-mysql pdo_pgsql
 
 EXPOSE 80
 
--- files/cn.php
+++ files/cn.php
@@ -2,8 +2,8 @@
 $usuario = "root";
 $contrasena = "root";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.
 $host = "localhost";
-$puerto = "33007";
-if (!($link = mysql_connect($host.":".$puerto, $usuario, $contrasena)))
+$puerto = "3306";
+if (!($link = mysqli_connect($host.":".$puerto, $usuario, $contrasena)))
  {
     echo "Error conectando a la base de datos.<br>";
     exit();
